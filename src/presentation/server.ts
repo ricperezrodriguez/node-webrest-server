134a4c6c5afb96eec826d11d003c675fcc7c9697
Ricardo Perez <rperezrodriguez@deloitte.es>
2024-05-09 10:24:27 +0200

    rest

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,27 +1,36 @@
-import express from "express";
+import express, { Router } from "express";
 import path from "path";
 
 interface Options {
   port: number;
   public_path?: string;
+  routes: Router;
 }
 export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(option: Options) {
-    const { port, public_path = "public" } = option;
+    const { port, public_path = "public", routes } = option;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   async start() {
     // Middlewares
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
 
     // Public Folder
     this.app.use(express.static("public"));
 
+    // Routes
+    this.app.use(this.routes);
+
+    // SPA
     this.app.get("*", (req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`
